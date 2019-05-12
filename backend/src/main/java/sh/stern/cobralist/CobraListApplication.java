f5bf6482e63aa89be0c666438a66edd78f4b5107
DK-Stern <github@stern.sh>
2019-05-12 17:55:37 +0200

    init
    * Spring Boot und Angular Modul angelegt
    * Spring Boot mit Angular konfiguriert
    ** Angular Anwendung wird in die Spring Boot Anwendung beim bauen eingebettet
    * AsciiDoctor Modul angelegt und konfiguriert
    ** Adoc Dateien werden in Latex konvertiert und in die BA eingebettet

---
+++ backend/src/main/java/sh/stern/cobralist/CobraListApplication.java
@@ -0,0 +1,13 @@
+package sh.stern.cobralist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobraListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CobraListApplication.class, args);
+    }
+
+}
