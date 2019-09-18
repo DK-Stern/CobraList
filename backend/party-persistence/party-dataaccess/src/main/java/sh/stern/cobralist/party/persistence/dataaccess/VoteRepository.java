99475f021fa91c75922fc6b4955d43eb453d9a17
dk-stern <gitkraken@stern.sh>
2019-09-18 18:29:27 +0200

    Code refactoring.

--- backend/party-persistence/party-dataaccess/src/main/java/sh/stern/cobralist/party/persistence/dataaccess/VoteRepository.java
+++ backend/party-persistence/party-dataaccess/src/main/java/sh/stern/cobralist/party/persistence/dataaccess/VoteRepository.java
@@ -5,8 +5,8 @@ import sh.stern.cobralist.party.persistence.domain.Vote;
 
 import java.util.List;
 
-@SuppressWarnings("squid:S00100")
 // query methods need underscores for attributes, so breaking java naming convention is ok
+@SuppressWarnings("squid:S00100")
 public interface VoteRepository extends JpaRepository<Vote, Long> {
     List<Vote> findByUser_Id(Long userID);
 
